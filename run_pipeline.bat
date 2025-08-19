c1a00b356234a7f1278123690cc5cd0da9c3e005
Philip Juhlin - Admin - SWE <admin.root.juhlin@intertek.com>
2025-08-19 08:00:48 +0200

    made a typo in the begining to pipline instead of pipeline....

--- run_pipeline.bat
+++ run_pipeline.bat
@@ -3,6 +3,6 @@ set BASEDIR=%~dp0
 
 if exist "%BASEDIR%stop.flag" del "%BASEDIR%stop.flag"
 
-"%BASEDIR%venv\Scripts\python.exe" "%BASEDIR%pipline.py"
+"%BASEDIR%venv\Scripts\python.exe" "%BASEDIR%pipeline.py"
 
 exit
